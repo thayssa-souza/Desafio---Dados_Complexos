4fc5d7c303516a7b9cd328ae4af7f770f64ff166
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2022-07-18 13:29:53 -0300

    Done

---
+++ script.js
@@ -0,0 +1,25 @@
+// Crie um array que receba 5 itens e exiba no console.
+let food = ['Arroz', 'Feijão', 'Macarrão', 'Frango assado', 'Carne']
+console.log(food)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+food.unshift('Lasanha')
+console.log(food)
+
+// Utilize um método para remover o último nome do array.
+food.pop()
+console.log(food)
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+food = ['Arroz', 'Feijão', 'Macarrão', 'Frango assado', 'Carne']
+food.push('Salada', 'Purê')
+console.log(food)
+
+// Utilize um método para remover o primeiro nome do array.
+food.shift()
+console.log(food)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
